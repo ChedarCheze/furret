649acc7ee9c1b7fb1c553af2a025e05d30819d11
marcdatboi <marcmauricebacongcobell@gmail.com>
2026-03-16 16:32:10 -0700

    Added multiplication and division functions

--- main.cpp
+++ main.cpp
@@ -5,6 +5,20 @@
 #include <iostream>
 #include <string>
 
+// Multiply
+int multiply(int a, int b) {
+    return a + b;
+}
+
+// Division Function
+int divide(int a, int b) {
+
+    if (b == 0) {
+        std::cerr << "Cannot divide by 0!\n"; // Displays an error message
+        exit(1); // Terminates program with an exit code of 1
+    }
+}
+
 int main() {
     std::string eggbert = "leggbert";
     std::cout << "Hello World!\n";
