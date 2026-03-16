6ec40312045e32c1aaf072aaf7445c1382f0028e
ChedarCheze <kingsley.wu760@gmail.com>
2026-03-16 16:34:37 -0700

    2 funcgtiosn

--- main.cpp
+++ main.cpp
@@ -5,6 +5,16 @@
 #include <iostream>
 #include <string>
 
+
+int addition(int a, int b) {
+    return a + b;
+}
+
+
+int subtraction(int a, int b) {
+    return a - b;
+}
+
 int main() {
     std::string eggbert = "leggbert";
     std::cout << "Hello World!\n";
