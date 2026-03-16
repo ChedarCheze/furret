7243818d4b9e8088604b1d4de7375e5b62759668
ChedarCheze <kingsley.wu760@gmail.com>
2026-03-16 16:38:39 -0700

    -m

--- main.cpp
+++ main.cpp
@@ -13,6 +13,19 @@ int addition(int a, int b) {
 
 int subtraction(int a, int b) {
     return a - b;
+
+// Multiply
+int multiply(int a, int b) {
+    return a + b;
+}
+
+// Division Function
+int divide(int a, int b) {
+
+    if (b == 0) {
+        std::cerr << "Cannot divide by 0!\n"; // Displays an error message
+        exit(1); // Terminates program with an exit code of 1
+    }
 }
 
 int main() {
